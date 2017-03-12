d07722f89c44c9527fa2626cf6f642781994b4db
kingcc <laikinfox@gmail.com>
2017-03-12 22:05:16 +0800

    script auto commit at [2017-03-12][22:05:16.355283156]

---
+++ creeperItem.js
@@ -0,0 +1,9 @@
+class creeperItem {
+  constructor({ name = '', url = '', tags = 'h1' }) {
+    this.name = name,
+      this.url = url,
+      this.tags = tags;
+  }
+}
+
+module.exports = creeperItem;
